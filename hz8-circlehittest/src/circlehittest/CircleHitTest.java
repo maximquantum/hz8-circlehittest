8181ad742ddde20b4427e3e4568e22a4f7e55ded
maximquantum <maximquantum148@gmail.com>
2024-06-05 21:15:35 +0200

    hc oplossing

--- hz8-circlehittest/src/circlehittest/CircleHitTest.java
+++ hz8-circlehittest/src/circlehittest/CircleHitTest.java
@@ -1,36 +1,48 @@
 package circlehittest;
 
 abstract class CircleHitTest {
-	
+	/**
+	 * @pre | c != null
+	 * @post Als het punt binnen de cirkel ligt, is `result` gelijk aan `true`
+	 *       | !(c.centre.squareDistanceTo(p) <= c.radius * c.radius) || result == true
+	 * @post Als het punt buiten de omschreven rechthoek ligt, is `result` gelijk aan `false`
+	 *       | !(c.centre.x - c.radius <= p.x && p.x <= c.centre.x + c.radius &&
+		     | c.centre.y - c.radius <= p.y && p.y <= c.centre.y + c.radius) || result == false
+	 */
 	abstract boolean containsPoint(Circle c, Point p);
 }
 
 class PreciseCircleHitTest extends CircleHitTest {
+	/**
+	 * @throws IllegalArgumentException | c == null
+	 * @post | result == (c.centre.squareDistanceTo(p) <= c.radius * c.radius)
+	 */
 	
+	@Override
 	boolean containsPoint(Circle c, Point p) {
-		if ( Math.sqrt((p.x - c.centre.x)^2 + (p.x - c.centre.x)^2) == c.radius)
-			return true;
-		else
-			return false;
+		return c.centre.squareDistanceTo(p) <= c.radius * c.radius;
 	}
 }
 
 class FastCircleHitTest extends CircleHitTest {
-	
 	/**
-	 * 
+	 * @pre | c != null
+	 * @post | result == (c.centre.x - c.radius <= p.x && p.x <= c.centre.x + c.radius &&
+			 | c.centre.y - c.radius <= p.y && p.y <= c.centre.y + c.radius)
 	 */
+	@Override
 	boolean containsPoint(Circle c, Point p) {
-		if ( Math.abs(p.x - c.centre.x) <= c.radius && Math.abs(p.y - c.centre.y) <= c.radius)
-			return true;
-		else
-			return false;
+		return c.centre.x - c.radius <= p.x && p.x <= c.centre.x + c.radius &&
+				c.centre.y - c.radius <= p.y && p.y <= c.centre.y + c.radius;
 	}
 }
 
 final class Point {
 	
 	int x, y;
+	int squareDistanceTo(Point other) {
+		return (x - other.x) * (x - other.x) + (y - other.y) * (y - other.y);
+	}
 
 	public Point(int x, int y) {
 		this.x = x;
